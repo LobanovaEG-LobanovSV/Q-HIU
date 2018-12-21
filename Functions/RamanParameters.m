5d5ff9c260a5e88a48c8877a364d642a12a8bd90
LobanovaEG-LobanovSV <38347132+LobanovaEG-LobanovSV@users.noreply.github.com>
2018-12-21 22:53:04 +0000

    Add files via upload

--- Functions/RamanParameters.m
+++ Functions/RamanParameters.m
@@ -18,23 +18,24 @@
 %   dnu   - "one" pixel shift along spectral axis. Default is 1.
 %R_thr_SVD- mean autocorrelation coefficient threshold which splits the
 %           autocorrelation plane into noisy and non-noisy half-planes in
-%           the SVD-ACD procedure.
-%  sigma  - standard deviation, which is used for the BGF procedure.
-%R_thr_BGF- spectral autocorrelation coefficient threshold which is used
-%           in the the BGF procedure in order to discard the spatial
-%           points, Raman intensity of which have correlation values lower
-%           than R_thr_BGF.
-%  Save   - logical parameter, which specify to save initial spectra and
-%           results of SVD-ACD and BGF or not.
-%   nu2   - Spectral range used in the Q-US/PS-NMF procedure. If initial
-%           spectral range consists of one interval, nu2 in the
-%           dialog box can be written as two wavenumbers, for example,
-%           500, 4000. If initial spectral range consists of several
-%           intervals, square brackets must be used, for example,
-%           [500, 1000], [3000, 4000].
-%    N    - number of components in the Q-US/PS-NMF procedure.
-%  Niter  - number of iterations for the Q-US/PS-NMF procedure.
-%  Nrepl  - number of replications of the Q-US/PS-NMF procedure.
+%           the SVD-ADC analysis.
+%  sigma  - standard deviation, which is the only one parameter used for
+%           the BGF analysis.
+%R_thr_BGF- spectral autocorrelation coefficient threshold, which is an
+%           optional parameter used in the BGF analysis in order to discard
+%           the spatial points, Raman intensity of which have correlation
+%           values lower than R_thr_BGF.
+%  Save   - logical parameter, which specify whether to save raw mean
+%           spectra and the results of intermediate steps of SVD-ADC and
+%           BGF.
+%   nu2   - Spectral range used in the Q-US/PS-NMF analysis. If spectral
+%           range consists of one interval, nu2 in the dialog box can be
+%           written as two wavenumbers, for example, 500, 4000.
+%           If spectral range consists of several intervals, square
+%           brackets must be used, for example, [500, 1000], [3000, 4000].
+%    N    - number of components in the Q-US/PS-NMF analysis.
+%  Niter  - number of iterations for the Q-US/PS-NMF analysis.
+%  Nrepl  - number of replications of the Q-US/PS-NMF analysis.
 %    KS   - logical parameter, which specifies whether known spectra are
 %           used or not.
 %
